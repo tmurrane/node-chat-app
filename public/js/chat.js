0a43546f7c3062dc18bbb50b2136d7f69fb6fff5
Thomas Murrane <tmurrane@Thomas-Murranes-iMac.local>
2017-09-25 08:42:23 -0500

    Send messages to people in room

--- public/js/chat.js
+++ public/js/chat.js
@@ -57,8 +57,6 @@ socket.on('newMessage', function (message) {
 });
 
 socket.on('newLocationMessage', function (message) {
-  // var li = jQuery('<li></li>');
-  // var a = jQuery('<a target="_blank">My current location</a>');
   var template = jQuery('#location-message-template').html();
   var formattedTime = moment(message.createdAt).format('h:mm a');
   var html = Mustache.render(template, {
@@ -66,9 +64,6 @@ socket.on('newLocationMessage', function (message) {
     createdAt: formattedTime,
     url: message.url
   });
-  // li.text(`${message.from} ${formattedTime}: `);
-  // a.attr('href', message.url);
-  // li.append(a);
   jQuery('#messages').append(html);
   scrollToBottom();
 });
@@ -78,7 +73,6 @@ jQuery('#message-form').on('submit', function (e) {
 
   var messageTextbox = jQuery('[name=message]');
   socket.emit('createMessage', {
-    from: 'User',
     text: messageTextbox.val()
   }, function () {
     messageTextbox.val('')
